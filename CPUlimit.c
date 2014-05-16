30451b65cdd5aa2f3fad33dc126adb761571c7a5
aisberg <ais-berg@mail.ru>
2014-05-16 08:36:42 +0300

    Update CPUlimit.c

--- CPUlimit.c
+++ CPUlimit.c
@@ -1,3 +1,11 @@
+//
+// main.c
+// CPU
+//
+// Created by Макс on 24.03.14.
+// Copyright (c) 2014 Максим Никитин. All rights reserved.
+//
+
 #include<stdio.h>
 #include<fcntl.h>
 #include<signal.h>
